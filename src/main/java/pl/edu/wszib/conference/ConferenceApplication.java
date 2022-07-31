10dc5501e317054b91a8d6d70536da0cecfe5dce
Anouk <anuklza@gmail.com>
2022-07-31 21:34:28 +0200

    Konfiguracja bazy danych MS SQL - dodano @PropertySource

--- src/main/java/pl/edu/wszib/conference/ConferenceApplication.java
+++ src/main/java/pl/edu/wszib/conference/ConferenceApplication.java
@@ -2,8 +2,11 @@ package pl.edu.wszib.conference;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:db.properties")
+
 public class ConferenceApplication {
 
     public static void main(String[] args) {
